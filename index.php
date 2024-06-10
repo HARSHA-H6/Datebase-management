9a4aff9a31d8aa11f6d9fac36bc1d717b87c75a3
HARSHA.H <113204434+HARSHA-H6@users.noreply.github.com>
2024-06-10 14:58:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Transport</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  
+</head>
+
+<body id="body">
+
+  <!--==========================
+    Top Bar
+  ============================-->
+  <section id="topbar" class="d-none d-lg-block">
+    <div class="container clearfix">
+      <div class="contact-info float-left">
+        <i class="fa fa-envelope-o"></i> <a href="mailto:contact@example.com">hrstravals@gmail.com</a>
+        <i class="fa fa-phone"></i> +91 9986295866
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="#body" class="scrollto">HRS<span>Travels</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="#body"><img src="img/logo.png" alt="" title="" /></a>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#body">Home</a></li>
+          <li><a href="#about">About Us</a></li>
+          
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="registration/admin.php">Admin</a></li>
+          <li><a href="registration/login.php">login</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <section id="intro">
+
+    <div class="intro-content">
+      <h2>Making <span>your Ride</span><br>happen!</h2>
+      <div>
+        <a href="profile.php" class="btn-get-started scrollto">Ticket Book</a>
+      </div>
+    </div>
+
+    <div id="intro-carousel" class="owl-carousel" >
+      <div class="item" style="background-image: url('img/bus1.jpg');"></div>
+      <div class="item" style="background-image: url('img/bus2.jpg');"></div>
+      <div class="item" style="background-image: url('img/bus3.jpg');"></div>
+      <div class="item" style="background-image: url('img/bus4.jpg');"></div>
+      <div class="item" style="background-image: url('img/bus5.jpg');"></div>
+      <div class="item" style="background-image: url('img/bus6.jpg');"></div>
+      <div class="item" style="background-image: url('img/bus7.jpg');"></div>
+      <div class="item" style="background-image: url('img/intro-carousel/5.jpg');"></div>
+    </div>
+
+  </section><!-- #intro -->
+
+  <main id="main">  
+    <section id="portfolio" class="wow fadeInUp">
+      <div class="container">
+        <div class="section-header">
+          <h2>Our Portfolio</h2>
+          <p>WE ARE THE LEADING TRAVEL AGENCY IN INDIA WE OPREATE ACROSSS INDIA
+            <br>With 10M+ happy cutomers and 5 years of experience </p>
+        </div>
+      </div>
+
+      <div class="container-fluid">
+        <div class="row no-gutters">
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/port1.jpg" class="portfolio-popup">
+                <img src="img/port1.jpg" alt="" style="height:350px;">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp"></h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/port2.jpeg" class="portfolio-popup">
+                <img src="img/port2.jpeg" alt="" style="height:350px;">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp"></h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/port3.jpg" class="portfolio-popup">
+                <img src="img/port3.jpg" alt=""style="height:350px;">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp"></h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/port4.jpg" class="portfolio-popup">
+                <img src="img/port4.jpg" alt=""style="height:350px;">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp"></h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/port5.jpg" class="portfolio-popup">
+                <img src="img/port5.jpg" alt=""style="height:350px;">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp"></h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/port6.jpg" class="portfolio-popup">
+                <img src="img/port6.jpg" alt=""style="height:350px;">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp"></h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/port7.jpg" class="portfolio-popup">
+                <img src="img/port7.jpg" alt=""style="height:350px;">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp"></h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="portfolio-item wow fadeInUp">
+              <a href="img/port9.jpg" class="portfolio-popup">
+                <img src="img/port9.jpg" alt=""style="height:350px;">
+                <div class="portfolio-overlay">
+                  <div class="portfolio-info"><h2 class="wow fadeInUp"></h2></div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #portfolio -->
+     <section id="Partners" class="wow fadeInUp">
+      <div class="container">
+        <div class="section-header">
+          <h2>Partners</h2>
+          <p>With there Help and Corporation we make your travel full of Joy and Memorable</p>
+        </div>
+
+        <div class="owl-carousel clients-carousel">
+          <img src="img/partner1.png" alt="">
+          <img src="img/partners2.jpeg" alt="">
+          <img src="img/partner3.jpg" alt="">
+          <img src="img/partner4.jpg" alt="">
+          <img src="img/partner5.png" alt="">
+        </div>
+
+      </div>
+    </section>
+
+    <!--==========================
+      About Section
+    ============================-->
+    <section id="about" class="wow fadeInUp">
+      <div class="container">
+        <div class="section-header">
+        <h2>About Us</h2>
+        <p>Established in 2019, HRSTravals has positioned itself as one of the leading companies,
+               providing great offers, competitive fares, exclusive discounts, and a seamless online booking experience to many of its customers.
+             The experience of booking your  tickets, hotel stay, and holiday package through our desktop site or mobile app can be done with complete ease and no hassles at all. 
+             We also deliver amazing offers, such as Instant Discounts, Fare Calendar, RewardsProgram, MyWallet, and many more while updating them from time to time to better suit our customers’
+              evolving needs and demands. </p>        </div>
+
+        <div class="owl-carousel clients-carousel">
+          <img src="img/discount.png" alt="">
+          <img src="img/rewards.png" alt="">
+          <img src="img/rating.png" alt="">
+          <img src="img/cashback.png" alt="">
+          <img src="img/easy.png" alt="">
+        </div>
+
+      </div>
+    </section>
+    <section id="about" class="wow fadeInUp">
+      <div class="container">
+        <div class="section-header">
+         
+         
+            <h4><b>Booking tickets with HRSTravals</b></h4></th>
+
+        
+              <img src="team-1.jpg" alt="">
+           
+            <p>
+              At HRSTravals, you can find the best of deals and cheap tickets to any place you want by booking your tickets on 
+              our website. Being India’s leading website for hotel, and holiday bookings,  helps you book  tickets that are 
+              affordable and customized to your convenience. With customer satisfaction being our ultimate goal, we also have a 24/7 dedicated helpline
+               to cater to our customer’s queries and concerns. Serving over 10 million happy customers, we at HRSTravals are glad to fulfill 
+               the dreams of folks who need a quick and easy means to find  tickets.
+                You can get a hold of the cheapest prices of your choice today while also enjoying the other available options for your travel 
+                 needs with us.</p>
+
+            </div>
+    </section>
+   <!-- #about -->
+
+   
+
+   
+    
+  </main>
+
+  
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>HRSTravals</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+       
+        Designed by <a href="#">Infinite Network Solutions</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="lib/sticky/sticky.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
